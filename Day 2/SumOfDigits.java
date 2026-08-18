bd938abd5a660579dfa46763ca2c1b373fff2354
Rajshar <rajashar4803@gmail.com>
2026-08-18 13:10:53 +0530

    session 2 day 2

--- Day 2/SumOfDigits.java
+++ Day 2/SumOfDigits.java
@@ -1,4 +1,28 @@
-//
+import java.util.Scanner;
+
 public class SumOfDigits {
-    
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        System.out.println("Enter a number greater than 100");
+        long no = sc.nextLong();
+        System.out.println("The sum of digits of "+ no + "is "+sod(no));
+        sc.close();
+    }
+    public static int sod(int no){
+        
+        int sum=0;
+        while(no>0){
+            sum+= no%10;
+            no/= 10;
+        }
+        return sum;
+
+    }
+
+    public static long sod(long no){
+        if(no<10) return no;
+        if(no<=0) return 0;
+        return no%10+sod(no/10);
+
+    }
 }
